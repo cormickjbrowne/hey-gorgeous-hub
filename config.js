b90549eede2452d29b342c3302858ca8d0414d61
Cormick Browne <cormick@heygorgeous.com>
2016-03-21 18:39:56 -0400

    Add sanitized config file

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    "shop": process.env.SHOP,
+    "shopify_api_key": process.env.SHOPIFY_API_KEY,
+    "access_token": process.env.ACCESS_TOKEN,
+    "verbose": process.env.VERBOSE
+};
