516f87148c8875d7c5d066a8ed3e342027ce8082
Cormick Browne <cormick@heygorgeous.com>
2016-04-05 18:09:14 -0400

    Create tests for utils.hashArray and utils.removeSoldOutSizeTags

--- tests/test-utils.js
+++ tests/test-utils.js
@@ -257,5 +257,74 @@ describe('#removeSoldOutSizeTags()', function () {
         tags = utils.removeSoldOutSizeTags(product).tags;
         tags.should.not.contain('{size:12}');
         tags.should.contain('{size:14}');
+    });
+    it('it should handle case where no metafields exist', function () {
+        var product, tags;
+
+        product = {
+            tags: '{size:12}, {size:14}, ShowOnHG',
+            variants: [
+                {
+                    id: 6442490753,
+                    inventory_policy: 'deny',
+                    inventory_quantity: -1,
+                    metafields: []
+                },
+                {
+                    id: 6442490754,
+                    inventory_policy: 'deny',
+                    inventory_quantity: 1,
+                    metafields: []
+                },
+                {
+                    id: 6442490755,
+                    inventory_policy: 'deny',
+                    inventory_quantity: 1,
+                    metafields: []
+                }
+            ]
+        };
+
+        tags = utils.removeSoldOutSizeTags(product).tags;
+        tags.should.contain('{size:12}');
+        tags.should.contain('{size:14}');
+    });
+    it('it should handle the case where all variants are sold out', function () {
+        var product, tags;
+
+        product = {
+            tags: '{size:12}, {size:14}, ShowOnHG',
+            variants: [
+                {
+                    id: 6442490755,
+                    inventory_policy: 'deny',
+                    inventory_quantity: 0,
+                    metafields: [
+                        {
+                            "id":19704969480,
+                            "namespace":"Variant",
+                            "key":"sizes",
+                            "value":"14",
+                            "value_type":"string",
+                            "description":null,
+                            "owner_id":6442490754,
+                            "created_at":"2016-04-04T16:46:04-04:00",
+                            "updated_at":"2016-04-04T16:46:04-04:00",
+                            "owner_resource":"variant"
+                        }
+                    ]
+                }
+            ]
+        };
+
+        tags = utils.removeSoldOutSizeTags(product).tags;
+        tags.should.contain('{size:12}');
     })
+});
+describe('#hashArray', function () {
+    it('it should be return an empty object if array is empty', function () {
+        var result = utils.hashArray([]);
+        console.log("result:", result);
+        console.log("result[123]:", result[123]);
+    });
 });
